36977e26a005f9f1143996b881d53e733dc6d1e2
jc_local_pupo <arhuaco@gmail.com>
2022-05-01 23:37:59 -0500

    se lista elemtos y se hace filtro para mostrar totales de tipo de cafe

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -7,18 +7,28 @@ import { CafeService } from '../cafe.service';
   templateUrl: './cafe-list.component.html',
   styleUrls: ['./cafe-list.component.css']
 })
+
 export class CafeListComponent implements OnInit {
 
+
+  totalTipoCafeArabico:number=0;
+  totalTipoCafeBlend:number=0;
+  logoCafe:string="assets/cafebanner.PNG";
   cafes: Array<Cafe> = [];
   constructor(private cafeService: CafeService) { }
+
 
   getTipoCafe(): void {
     this.cafeService.getTipoCafe().subscribe((cafes) => {
       this.cafes = cafes;
+      this.totalTipoCafeBlend=cafes.filter((cafe) => cafe.tipo === "Blend").length;//cafes.filter(item => item.nombre.toString().toUpperCase() === 'Blend'.toUpperCase()).length;
+      this.totalTipoCafeArabico=cafes.filter(t=>t.tipo ==='Café de Origen').length;
     });
+
   }
 
   ngOnInit() {
+
     this.getTipoCafe();
   }
 
